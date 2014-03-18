e62986e772e01c3f8d7cee8da572ef8e5fd57a0c
Shawn Neal <sneal@sneal.net>
2014-03-18 08:02:50 -0700

    Validate knife config file exists if specified

--- lib/vagrant-chefconfig/config.rb
+++ lib/vagrant-chefconfig/config.rb
@@ -14,6 +14,18 @@ module Vagrant
         @enabled = true if @enabled == UNSET_VALUE
         @knife_config_path = nil if @knife_config_path == UNSET_VALUE
       end
+
+        def validate(machine)
+          errors =[]
+
+          if @enabled
+            if @knife_config_path && !File.exists?(@knife_config_path)
+              errors << "chefconfig.knife_config_path file '#{@knife_config_path}' not found" 
+            end
+          end
+
+          { "chefconfig" => errors }
+        end
     end
   end
 end
